54d9251ae734b54c9e6fe091a1dafae6eafad4a1
deeksha-aashri <deekshaaashri1@gmail.com>
2022-11-15 23:49:36 +0530

    :sparkles: mounting in expree

--- views/method.js
+++ views/method.js
@@ -1,9 +1,89 @@
 
-//Http methods and using postman
+// //Http methods and using postman
 
+// const express=require('express');
+// let app=express();
+// app.use(express.json());//write it to use post method
+// let user = [
+//     {
+//       id: 1,
+//       name: "Abhishek",
+//       age: 100,
+//     },
+//     {
+//       id: 2,
+//       name: "Rajat",
+//       age: 10,
+//     },
+//     {
+//       id: 3,
+//       name: "Sunjyot",
+//       age: 50,
+//     },
+//   ];
+//   //with query
+//   app.get('/user', (req, res) => {
+//       res.send(user);
+//       console.log(req.query);
+//       let { name, age } = req.query;
+//       let filteredData=user.filter(userObj => {
+//           return (userObj.name==name && userObj.age==age)
+//       })
+//       res.send(filteredData);
+//   })
+
+
+// app.get('/user', (req,res)=>{
+// res.send(user)
+// })
+
+// app.post('/user', (req, res) => {
+//     console.log(req.body.Name);
+//     //Putting the data sent via req in db
+//     user = req.body;
+//     res.json({
+//         message: "Data received successfully",
+//         user: req.body
+//     });
+// });
+
+// app.patch('/user', (req, res) => {
+//     console.log(req.body);
+//     let dataToBeUpdated = req.body;
+//     for (key in dataToBeUpdated) {
+//         user[key] = dataToBeUpdated[key];
+//     }
+//     res.json({
+//         message: "data updated succesfully"
+//     })
+// });
+
+// app.delete('/user', (req, res) => {
+//     user = {};
+//     res.json({
+//         msg: "user has been deleted"
+//     });
+// })
+
+// //params
+// app.get('/user/:name', (req, res) => {
+//     console.log(req.params.name);
+//     //let {id}=req.params;
+//     // let user = db.findOne(id);
+//     res.json({ msg: "user id is ", "obj": req.params });
+// });
+
+// app.listen(5000,()=>{
+//     console.log("Listening at port 5000")
+// })
+
+
+
+//USing mounting in express
 const express=require('express');
-let app=express();
-app.use(express.json());//write it to use post method
+const app=express();
+app.use(express.json());
+
 let user = [
     {
       id: 1,
@@ -21,59 +101,60 @@ let user = [
       age: 50,
     },
   ];
-  //with query
-  app.get('/user', (req, res) => {
-      res.send(user);
-      console.log(req.query);
-      let { name, age } = req.query;
-      let filteredData=user.filter(userObj => {
-          return (userObj.name==name && userObj.age==age)
-      })
-      res.send(filteredData);
-  })
+
+  //step one to  mounting
+  const userRouter=express.Router();
+  app.use('/user', userRouter);//significance?
 
 
-app.get('/user', (req,res)=>{
-res.send(user)
-})
+  //2.
+  userRouter.route("/").get(getUser).post(postUser).patch(updateUser).delete(deleteUser)
 
-app.post('/user', (req, res) => {
+  //correct way of writing is as follows
+  userRouter
+          .route('/:name')
+          .get(getUserByName);
+
+function getUser(req,res){
+    console.log(req.query);
+    let { name, age } = req.query;
+   
+    // let filteredData=user.filter(userObj => {
+    //     return (userObj.name==name && userObj.age==age)
+    // })
+    // res.send(filteredData);
+    res.send(user);
+}
+
+function postUser(req,res){
     console.log(req.body.Name);
-    //Putting the data sent via req in db
-    user = req.body;
+    user.push(req.body);
     res.json({
         message: "Data received successfully",
         user: req.body
     });
-});
+}
 
-app.patch('/user', (req, res) => {
+function updateUser(req,res){
     console.log(req.body);
-    let dataToBeUpdated = req.body;
-    for (key in dataToBeUpdated) {
-        user[key] = dataToBeUpdated[key];
-    }
-    res.json({
-        message: "data updated succesfully"
-    })
-});
+    let dataToBeUpdated=req.body;
+    for(key in dataToBeUpdated){
 
-app.delete('/user', (req, res) => {
+    }
+}
+function deleteUser(req,res){
     user = {};
     res.json({
         msg: "user has been deleted"
     });
-})
+}
 
-//params
-app.get('/user/:name', (req, res) => {
+
+function getUserByName(req, res){
     console.log(req.params.name);
     //let {id}=req.params;
     // let user = db.findOne(id);
     res.json({ msg: "user id is ", "obj": req.params });
-});
-
-app.listen(5000,()=>{
-    console.log("Listening at port 5000")
-})
+}
+app.listen(5000)
 
