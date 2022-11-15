4e40220488c2de7e5f7455c6472aa62ae6b84802
deeksha-aashri <deekshaaashri1@gmail.com>
2022-11-15 23:07:42 +0530

    :fire: method.js and readme.md updated

--- views/method.js
+++ views/method.js
@@ -1,8 +1,37 @@
+
 //Http methods and using postman
-let user={};
+
 const express=require('express');
 let app=express();
 app.use(express.json());//write it to use post method
+let user = [
+    {
+      id: 1,
+      name: "Abhishek",
+      age: 100,
+    },
+    {
+      id: 2,
+      name: "Rajat",
+      age: 10,
+    },
+    {
+      id: 3,
+      name: "Sunjyot",
+      age: 50,
+    },
+  ];
+  //with query
+  app.get('/user', (req, res) => {
+      res.send(user);
+      console.log(req.query);
+      let { name, age } = req.query;
+      let filteredData=user.filter(userObj => {
+          return (userObj.name==name && userObj.age==age)
+      })
+      res.send(filteredData);
+  })
+
 
 app.get('/user', (req,res)=>{
 res.send(user)
@@ -35,6 +64,15 @@ app.delete('/user', (req, res) => {
         msg: "user has been deleted"
     });
 })
+
+//params
+app.get('/user/:name', (req, res) => {
+    console.log(req.params.name);
+    //let {id}=req.params;
+    // let user = db.findOne(id);
+    res.json({ msg: "user id is ", "obj": req.params });
+});
+
 app.listen(5000,()=>{
     console.log("Listening at port 5000")
 })
--- views/readme.md
+++ views/readme.md
@@ -1,3 +1,7 @@
 Reading material
 
 https://medium.com/dlt-labs-publication/package-json-vs-package-lock-json-c8d5deba12cb
+
+API https://geetima.medium.com/how-do-you-explain-api-to-a-5-year-old-7d70f6986a15
+
+REST API https://www.edureka.co/blog/what-is-rest-api/
